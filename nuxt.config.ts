14d5204ee63bca26a21c5acb8720b318f4929e0a
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-10-12 17:19:42 -0300

    disable devtools

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   future: {
     compatibilityVersion: 4
   },
