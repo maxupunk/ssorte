1d66f219e29222f004d1d622bc2abf2c2b2f31bc
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-10-14 15:56:25 -0300

    disable SSR

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,7 @@ import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  ssr: false,
   devtools: { enabled: false },
   future: {
     compatibilityVersion: 4
