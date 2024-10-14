241f42e1e283f3db073ae53ad86bfbe5aa19a241
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-10-14 16:02:01 -0300

    create system sort

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,4 +1,9 @@
 <template>
+  <!-- Overlay de Pausa -->
+  <v-overlay v-model="pause" class="align-center justify-center">
+    <span class="text-pause">Pausado</span>
+  </v-overlay>
+
   <v-row justify="center" class="ma-2">
     <v-col cols="6" class="text-center">
       <v-sheet color="success" class="head">
@@ -9,56 +14,210 @@
     </v-col>
     <v-col cols="6" class="text-center text-h3">
       <v-sheet color="success" class="head">
-        <v-card-title class="text-h3">Formula aplicada
+        <v-card-title class="text-h3">Fórmula matemática:
           <hr>
         </v-card-title>
-        <v-card-text class="text-h2">
-          <div>
-            <span style="color: red;">A</span> = 21
-            <span style="color: blue;">C</span> = 1
-            <span style="color: purple;">M</span> = 1000000
+        <v-card-text>
+          <div class="text-h4 ma-4">
+            <math xmlns="http://www.w3.org/1998/Math/MathML">
+              <mrow>
+                <mi class="text-light-blue-darken-4">a</mi>
+                <mo>= {{ calcA }}</mo>
+                <mi class="text-yellow-accent-4">numero</mi>
+                <mo>= {{ lotteryNumber }}</mo>
+                <mi class="text-cyan-darken-4">c</mi>
+                <mo>= {{ calcC }}</mo>
+                <mi class="text-deep-purple-accent-4">m</mi>
+                <mo>= {{ calcM }}</mo>
+              </mrow>
+            </math>
           </div>
-          <div>
-            (<span style="color: red;">A</span> x <span style="color: green;">NUMERO</span> +
-            <span style="color: blue;">C</span>) % <span style="color: purple;">M</span>
+          <div class="text-h2">
+            <math xmlns="http://www.w3.org/1998/Math/MathML">
+              <mrow>
+                <mo>(</mo>
+                <mi class="text-light-blue-darken-4">a</mi>
+                <mo>&#x22C5;</mo>
+                <mi class="text-yellow-accent-4">numero</mi>
+                <mo>+</mo>
+                <mi class="text-cyan-darken-4">c</mi>
+                <mo>)</mo>
+                <mo>mod</mo>
+                <mi class="text-deep-purple-accent-4">m</mi>
+              </mrow>
+            </math>
           </div>
         </v-card-text>
       </v-sheet>
     </v-col>
-    <v-col cols="12" class="text-center numGanhador" @click="startSort()">
-      <span class="gradient-text">
-        {{ winnerNumber }}
-      </span>
+    <v-col cols="12" class="text-center numGanhador">
+      {{ currentNumber }}
     </v-col>
     <v-col cols="12" class="text-center cor-preto pa-8">
       <v-img src="https://vippremiacoees.com/products/1711125215.png" height="230px" />
     </v-col>
   </v-row>
+
+  <v-dialog v-model="showNetworkErrorDialog" max-width="900">
+    <v-card>
+      <v-card-title class="headline">Erro de Conexão</v-card-title>
+      <v-card-text>
+        <h1>
+          Ocorreu um problema de conexão de rede. Por favor, verifique sua conexão e tente novamente.
+        </h1>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="primary" @click="showNetworkErrorDialog = false">
+          Fechar
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
+  <overlay-win :winner="winner" />
+
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import overlayWin from '~/components/overlayWin.vue';
 
-const lotteryNumber = ref('123456')
-const winnerNumber = ref('654321')
+const lotteryNumber = ref('596232')
+const currentNumber = ref()
+currentNumber.value = lotteryNumber.value
+const pause = ref(false)
+const play = ref(false)
+const showNetworkErrorDialog = ref(false);
+const timeSleep = ref(1000)
+const winner = ref({
+  number: '',
+  name: '',
+  date: '',
+  show: false,
+})
 
-// Função para atualizar o número do ganhador dinamicamente
-function updateWinnerNumber() {
-  winnerNumber.value = Math.floor(Math.random() * 999999)
+// Modulus
+const calcM = 1000000;
+// Multiplicador e incremento escolhidos para satisfazer condições LCG (Linear Congruential Generator) de período completo
+const calcA = 21;
+const calcC = 1;
+
+// função que aplica a Algoritmo do gerador congruencial linear
+function algorithm(number: string) {
+  const numberInt = parseInt(number);
+  // resultado da operação
+  const result = (calcA * numberInt + calcC) % calcM;
+  return result.toString().padStart(6, '0');
 }
 
-function startSort() {
-  console.log('Sorteando...')
+function handleKeyPress(event: KeyboardEvent) {
+  if (event.key === 'P' || event.key === 'p') {
+    pause.value = !pause.value;
+  }
+  if (event.key === 'S' || event.key === 's') {
+    startSort();
+  }
 }
 
-// Exemplo de atualização após 5 segundos
-setInterval(() => {
-  updateWinnerNumber()
-}, 100)
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyPress);
+});
+
+onBeforeUnmount(() => {
+  window.removeEventListener('keydown', handleKeyPress);
+});
+
+async function startSort() {
+  if (play.value) {
+    return
+  }
+  if (pause.value) {
+    pause.value = false;
+  }
+  currentNumber.value = lotteryNumber.value;
+  let generatedNumbers = new Set();
+  generatedNumbers.add(currentNumber.value);
+  // Loop infinito
+  play.value = true;
+  while (1) {
+    console.log("Numero sorteado: " + currentNumber.value);
+    if (pause.value) {
+      await sleep(100)
+      continue
+    }
+    if (timeSleep.value > 0) {
+      await sleep(timeSleep.value)
+      timeSleep.value -= 40
+    }
+    const isParticipant = await fetchSorte(currentNumber.value)
+    if (isParticipant && isParticipant.body) {
+      const nameWinner = isParticipant.body.orderproduct?.order?.customer?.name ? isParticipant.body.orderproduct?.order?.customer?.name : 'Nome do Ganhador'
+      const dateWinner = isParticipant.body.orderproduct?.order?.createdAt ? isParticipant.body.orderproduct?.order?.createdAt : '00/00/0000'
+      winner.value = {
+        number: isParticipant.body.number.toString(),
+        name: nameWinner,
+        date: dateWinner,
+        show: true,
+      }
+
+      break
+    }
+    // Apply the algorithm
+    let nextNumber = algorithm(currentNumber.value)
+
+    // Check if the number was already generated
+    if (generatedNumbers.has(nextNumber)) {
+      console.log(nextNumber + " repetido");
+    } else {
+      generatedNumbers.add(nextNumber);
+    }
+    // Update currentNumber
+    currentNumber.value = nextNumber;
+  }
+  play.value = false;
+}
+
+function sleep(ms: number) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+// Função para realizar a requisição GET
+async function fetchSorte(id: string) {
+  try {
+    return await $fetch(`/api/number/${id}`, {
+      retry: 999,
+      retryDelay: 600, // ms
+    })
+  } catch (error) {
+    pause.value = true;
+    showNetworkErrorDialog.value = true;
+  }
+}
 
 </script>
 
 <style scoped>
+.text-pause {
+  font-size: 100px;
+  font-weight: bold;
+  color: rgb(255, 255, 255);
+  text-shadow: 0px 0px 50px rgb(0, 0, 0);
+  animation: blinking 1s infinite;
+}
+
+@keyframes blinking {
+
+  0%,
+  100% {
+    opacity: 1;
+  }
+
+  50% {
+    opacity: 0;
+  }
+}
+
 .numLoteria {
   margin-top: -40px;
   font-size: 180px;
@@ -75,23 +234,5 @@ setInterval(() => {
 
 .cor-preto {
   background-color: black;
-}
-
-.gradient-text {
-  background: linear-gradient(90deg, green, #FFD500, red);
-  background-size: 200% 200%;
-  -webkit-background-clip: text;
-  color: transparent;
-  animation: gradientMove 3s linear infinite alternate;
-}
-
-@keyframes gradientMove {
-  0% {
-    background-position: 0% 50%;
-  }
-
-  100% {
-    background-position: 100% 50%;
-  }
 }
 </style>
