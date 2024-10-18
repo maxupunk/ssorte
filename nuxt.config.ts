af7da128915330d715f09c2f135f21eaa7756d54
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-10-18 11:18:50 -0300

    pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,19 @@
 import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify'
+import process from 'node:process'
+
+const sw = process.env.SW === 'true'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   ssr: false,
   devtools: { enabled: false },
   future: {
-    compatibilityVersion: 4
+    compatibilityVersion: 4,
+    typescriptBundlerResolution: true,
+  },
+  appConfig: {
+    buildDate: new Date().toISOString(),
   },
   runtimeConfig: {
     secretJwt: process.env.APP_SECRET_JWT,
@@ -21,7 +29,8 @@ export default defineNuxtConfig({
       })
     },
     //...
-    '@pinia/nuxt'
+    '@pinia/nuxt',
+    '@vite-pwa/nuxt'
   ],
   vite: {
     vue: {
@@ -30,5 +39,42 @@ export default defineNuxtConfig({
       },
     },
   },
-
+  // @ts-ignore
+  pwa: {
+    strategies: sw ? 'injectManifest' : 'generateSW',
+    srcDir: sw ? 'service-worker' : undefined,
+    filename: sw ? 'sw.ts' : undefined,
+    registerType: 'autoUpdate',
+    manifest: {
+      name: 'Vip premiacões',
+      short_name: 'VipPremiacoes',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: 'assets/icon512_maskable.png',
+          sizes: '512x512',
+          type: 'image/png',
+          purpose: 'any maskable',
+        },
+      ],
+    },
+    workbox: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    injectManifest: {
+      globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+    },
+    client: {
+      installPrompt: true,
+      // if enabling periodic sync for update use 1 hour or so (periodicSyncForUpdates: 3600)
+      periodicSyncForUpdates: 3600,
+    },
+    devOptions: {
+      enabled: true,
+      suppressWarnings: true,
+      navigateFallback: '/',
+      navigateFallbackAllowlist: [/^\/$/],
+      type: 'module',
+    },
+  },
 })
