ebdf26bc52f5c968c5be6488e3e78a6b42615291
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-10-18 18:43:18 -0300

    add pm2 in docker

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -2,7 +2,7 @@ module.exports = {
   apps: [
     {
       name: 'ssorte-Server',
-      port: '3000',
+      port: '3010',
       exec_mode: 'cluster',
       instances: 'max',
       script: './server/index.mjs'
