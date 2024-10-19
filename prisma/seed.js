8df3999b3bb6b8ef1d14e9be1c222513109f16fd
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-10-19 17:19:08 -0300

    fix conection

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,5 +1,15 @@
 import bcrypt from 'bcrypt'
-import prisma from "../server/prisma";
+import { PrismaClient } from '@prisma/client'
+import dotenv from 'dotenv';
+dotenv.config();
+
+const prisma = new PrismaClient({
+    datasources: {
+        db: {
+            url: process.env.DATABASE_URL
+        }
+    }
+})
 
 async function main() {
     const hashedPassword = await bcrypt.hash("admin", 10)
