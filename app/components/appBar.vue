fdf577d74865c88c0fae7fc9969f7e13c5b849c7
Maxuel Aguiar <maxupunk@yahoo.com.br>
2024-10-21 14:20:55 -0300

    disabled hidden toolbar

--- app/components/appBar.vue
+++ app/components/appBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar scroll-behavior="hide">
+  <v-app-bar>
     <v-app-bar-nav-icon @click="drawer = !drawer" v-if="token"></v-app-bar-nav-icon>
     <v-app-bar-title>
       {{ props.title }}
